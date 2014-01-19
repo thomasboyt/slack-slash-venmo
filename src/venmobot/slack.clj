2e13df227e837a51d10a887f2f119ced8eb1f0bb
Thomas Boyt <thomas.boyt@venmo.com>
2014-01-18 23:15:06 -0500

    Fake "make payment" behavior

---
+++ src/venmobot/slack.clj
@@ -0,0 +1,23 @@
+(ns venmobot.slack
+  (:require [clj-http.client :as client]
+            [clojure.data.json :as json]))
+
+(def webhook-url (System/getenv "WEBHOOK_URL"))
+
+(defn format-channel-name
+  "Add a hash (#) to a channel name if it doesn't have one"
+  [channel]
+  (cond
+    (not= (first channel) \#) (str "#" channel)
+    :else channel))
+
+(defn send-message
+  [channel message]
+  (let [json-message (json/write-str
+                       {:text message 
+                        :channel (format-channel-name channel)
+                        :username "venmobot"
+                        ;; TODO: probably move this image off imgur/make it not fucking huge
+                        "icon_url" "http://i.imgur.com/JknSeiD.png"})]
+    (client/post webhook-url
+                 {:body json-message :insecure? true})))
--- src/venmobot/venmo.clj
+++ src/venmobot/venmo.clj
@@ -1,5 +1,7 @@
 (ns venmobot.venmo
   (:require [venmobot.models.user :as user]
+            [venmobot.slack :as slack]
+            [clojure.string :as string]
             [clj-http.client :as client]
             [clojure.data.json :as json]))
 
@@ -16,19 +18,49 @@
 ;; Current token check does prevent other users from forging requests to make payments from an existing
 ;; user, but registration needs to be fixed!
 
+(defn make-payment
+  [username args channel]
+  (slack/send-message channel
+    (string/join "[fake] @" username " just paid " (args :recipient) " $" (args :amount) " for " (args :note)))
+  {:status 200})
+
+(defn parse-args
+  [text]
+  (let [pieces (string/split text #"\s+")]
+    ;; support either "@julian" or "julian"
+    {:recipient (cond (= (first (pieces 0)) \@)
+                      (subs (pieces 0) 1 (count (pieces 0)))
+                      :else
+                      (pieces 0))
+     ;; TODO validate this bit
+     :amount (pieces 1)
+     ;; support either "julian 1.00 for foo" or "julian 1.00 foo"
+     :note  (cond (= (pieces 2) "for")
+                  (string/join " " (drop 3 pieces))
+                  :else
+                  (string/join " " (drop 2 pieces)))}))
+
+(defn try-payment
+  [{username "user_name" text "text" channel "channel_name"}]
+  (if-let [args (parse-args text)]
+    (make-payment username args channel)
+    {:status 400 :body "Bad arguments"})
+  (slack/send-message channel "Testing!")
+  {:status 200})
+
 (defn do-venmo-transaction
   [{form-params :form-params}]
   (if-let [paying-user (user/get-user (form-params "user_name") (form-params "token"))]
-    {:body "alright, now make a payment"}
+    (try form-params)
     (render-oauth-link (form-params "user_name") (form-params "token"))))
 
 (defn oauth-callback
   [{params :params cookies :cookies}]
   (let [code (params "code")
         resp (client/post "https://api.venmo.com/v1/oauth/access_token"
-             {:form-params {:client_id (System/getenv "VENMO_CLIENT_ID")
-                            :client_secret (System/getenv "VENMO_CLIENT_SECRET")
-                            :code code}})
+               {:form-params {:client_id (System/getenv "VENMO_CLIENT_ID")
+                              :client_secret (System/getenv "VENMO_CLIENT_SECRET")
+                              :code code}})
         data (json/read-str (resp :body))
         user {:username ((cookies "slack_username") :value)
               :slacktoken ((cookies "slack_token") :value)
